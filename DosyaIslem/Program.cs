87b9089b391818093c9950d45026866c49dbe689
alicanbaydar <84176951+alicanbaydar@users.noreply.github.com>
2021-06-07 14:37:32 +0300

    Proje dosyası ekle.

---
+++ DosyaIslem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace DosyaIslem
+{
+    class Ekleme
+    {
+        private int sayi;
+        private string ad;
+        private string soyad;
+        public int Sayi { get {return sayi; } set {sayi=value; } }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ekleme ek = new Ekleme();
+            ek.Sayi = 1;
+            ek.Ad = "alican";
+            ek.Soyad = "baydar";
+
+            StreamWriter dosyaadi = File.CreateText(@"C:\Users\Atakan\Desktop\Dosyaad.txt");
+            dosyaadi.WriteLine("asdasdasdasd");
+            dosyaadi.WriteLine("xsxdxd");
+            dosyaadi.WriteLine(ek.Sayi + ek.Ad + ek.Soyad);
+            dosyaadi.Close();
+
+            StreamWriter dosyaadi2 = new StreamWriter(@"C:\Users\Atakan\Desktop\Dosyaad2.txt",true);
+            dosyaadi2.WriteLine("yeniler");
+            dosyaadi2.Close();
+        }
+    }
+}
