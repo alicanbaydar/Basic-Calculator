263a1a6320115c3a421873281fc99e76a0ad27bb
alicanbaydar <84176951+alicanbaydar@users.noreply.github.com>
2021-06-07 21:06:52 +0300

    TextYazmaOkuma

--- DosyaIslem/Program.cs
+++ DosyaIslem/Program.cs
@@ -8,27 +8,62 @@ namespace DosyaIslem
         private string ad;
         private string soyad;
         public int Sayi { get {return sayi; } set {sayi=value; } }
-        public string Ad { get; set; }
-        public string Soyad { get; set; }
+        public string Ad { get { return ad; } set{ad=value; }}
+        public string Soyad { get{return soyad; } set {soyad=value; } }
     }
+
+    class Meslek : Ekleme
+    {
+        public string meslek;
+        public Meslek(string meslek)
+        {
+            this.meslek = meslek;
+        }        
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Ekleme ek = new Ekleme();
             ek.Sayi = 1;
-            ek.Ad = "alican";
-            ek.Soyad = "baydar";
+            ek.Ad = "Alican";
+            ek.Soyad = "Baydar";
 
-            StreamWriter dosyaadi = File.CreateText(@"C:\Users\Atakan\Desktop\Dosyaad.txt");
-            dosyaadi.WriteLine("asdasdasdasd");
-            dosyaadi.WriteLine("xsxdxd");
-            dosyaadi.WriteLine(ek.Sayi + ek.Ad + ek.Soyad);
+            //Her çalıştırıldığında Dosyaad isimli dosyaya yeniden yazılma amacıyla file.createtext
+            //Dosya yolu değiştirilip çalıştırılabilir
+            string dosyayolu1 = @"C:\Users\Atakan\Desktop\Dosyaad.txt";
+            StreamWriter dosyaadi = File.CreateText(dosyayolu1);
+            Console.WriteLine("Alican Baydar için meslek giriniz! ");
+            string meslek1 = Console.ReadLine();
+            dosyaadi.WriteLine(ek.Sayi +" "+ ek.Ad +" "+ ek.Soyad +".  Meslek: "+ meslek1);
             dosyaadi.Close();
+            Console.WriteLine("1.Dosyaya bilgiler yazıldı");
 
-            StreamWriter dosyaadi2 = new StreamWriter(@"C:\Users\Atakan\Desktop\Dosyaad2.txt",true);
-            dosyaadi2.WriteLine("yeniler");
-            dosyaadi2.Close();
+            //Her seferinde Dosyaad1 dosyası üzerine yazılması amacıyla
+            Ekleme ek1 = new Ekleme { Sayi = 3, Ad = "Aylin", Soyad = "Saygın" };
+            string dosyayolu2 = @"C:\Users\Atakan\Desktop\Dosyaad1.txt";
+            StreamWriter dosyaadi1 = new StreamWriter(dosyayolu2, true);
+            dosyaadi1.WriteLine(ek1.Sayi +" " + ek1.Ad +" " + ek1.Soyad);
+            Console.WriteLine("2.Dosya için 1. kişinin verileri girildi");
+            
+            Meslek mslk = new Meslek("Doktor");
+            mslk.Sayi = 2;
+            mslk.Ad = "Hakan";
+            mslk.Soyad = "Uygun";
+  
+            dosyaadi1.WriteLine(mslk.Sayi +" "  + mslk.Ad +" "+ mslk.Soyad +". " + "Meslek: "+ mslk.meslek);
+            dosyaadi1.Close();
+            Console.WriteLine("2.Dosyadaki 2.kişinin verileri girildi \n");
+            
+            //Dosya okuma işlemleri..
+            StreamReader oku = new StreamReader(dosyayolu1);
+            string okunan = oku.ReadToEnd();
+            Console.WriteLine("1.Dosya okundu! Bilgiler: "+ okunan);
+
+            StreamReader oku2 = new StreamReader(dosyayolu2);
+            string okunan2 = oku2.ReadToEnd();
+            Console.WriteLine("2.Dosya Okundu! Bilgiler: \n" + okunan2);
         }
     }
 }
