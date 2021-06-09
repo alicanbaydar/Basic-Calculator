a4a1fd15eba1dff40f246c4bd7ae089d6d606321
alicanbaydar <alican.baydar94@gmail.com>
2021-06-09 20:09:11 +0300

    eklendi!

---
+++ HesapMakinesi/HesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿
+namespace HesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.number1 = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.Button();
+            this.number3 = new System.Windows.Forms.Button();
+            this.carpma = new System.Windows.Forms.Button();
+            this.number4 = new System.Windows.Forms.Button();
+            this.number5 = new System.Windows.Forms.Button();
+            this.number6 = new System.Windows.Forms.Button();
+            this.bolme = new System.Windows.Forms.Button();
+            this.number7 = new System.Windows.Forms.Button();
+            this.number8 = new System.Windows.Forms.Button();
+            this.number9 = new System.Windows.Forms.Button();
+            this.toplama = new System.Windows.Forms.Button();
+            this.temizle = new System.Windows.Forms.Button();
+            this.number0 = new System.Windows.Forms.Button();
+            this.esittir = new System.Windows.Forms.Button();
+            this.cıkarma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.SandyBrown;
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 50);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // number1
+            // 
+            this.number1.BackColor = System.Drawing.Color.LightCoral;
+            this.number1.Location = new System.Drawing.Point(13, 87);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(51, 50);
+            this.number1.TabIndex = 1;
+            this.number1.Text = "1";
+            this.number1.UseVisualStyleBackColor = false;
+            this.number1.Click += new System.EventHandler(this.number1_Click);
+            // 
+            // number2
+            // 
+            this.number2.BackColor = System.Drawing.Color.LightCoral;
+            this.number2.Location = new System.Drawing.Point(70, 87);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(51, 50);
+            this.number2.TabIndex = 2;
+            this.number2.Text = "2";
+            this.number2.UseVisualStyleBackColor = false;
+            this.number2.Click += new System.EventHandler(this.number2_Click);
+            // 
+            // number3
+            // 
+            this.number3.BackColor = System.Drawing.Color.LightCoral;
+            this.number3.Location = new System.Drawing.Point(127, 87);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(51, 50);
+            this.number3.TabIndex = 3;
+            this.number3.Text = "3";
+            this.number3.UseVisualStyleBackColor = false;
+            this.number3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // carpma
+            // 
+            this.carpma.BackColor = System.Drawing.Color.OrangeRed;
+            this.carpma.Location = new System.Drawing.Point(184, 87);
+            this.carpma.Name = "carpma";
+            this.carpma.Size = new System.Drawing.Size(51, 50);
+            this.carpma.TabIndex = 4;
+            this.carpma.Text = "X";
+            this.carpma.UseVisualStyleBackColor = false;
+            this.carpma.Click += new System.EventHandler(this.carpma_Click);
+            // 
+            // number4
+            // 
+            this.number4.BackColor = System.Drawing.Color.LightCoral;
+            this.number4.Location = new System.Drawing.Point(13, 143);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(51, 50);
+            this.number4.TabIndex = 5;
+            this.number4.Text = "4";
+            this.number4.UseVisualStyleBackColor = false;
+            this.number4.Click += new System.EventHandler(this.number4_Click);
+            // 
+            // number5
+            // 
+            this.number5.BackColor = System.Drawing.Color.LightCoral;
+            this.number5.Location = new System.Drawing.Point(70, 143);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(51, 50);
+            this.number5.TabIndex = 6;
+            this.number5.Text = "5";
+            this.number5.UseVisualStyleBackColor = false;
+            this.number5.Click += new System.EventHandler(this.number5_Click);
+            // 
+            // number6
+            // 
+            this.number6.BackColor = System.Drawing.Color.LightCoral;
+            this.number6.Location = new System.Drawing.Point(127, 143);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(51, 50);
+            this.number6.TabIndex = 7;
+            this.number6.Text = "6";
+            this.number6.UseVisualStyleBackColor = false;
+            this.number6.Click += new System.EventHandler(this.number6_Click);
+            // 
+            // bolme
+            // 
+            this.bolme.BackColor = System.Drawing.Color.OrangeRed;
+            this.bolme.Location = new System.Drawing.Point(184, 143);
+            this.bolme.Name = "bolme";
+            this.bolme.Size = new System.Drawing.Size(51, 50);
+            this.bolme.TabIndex = 8;
+            this.bolme.Text = "/";
+            this.bolme.UseVisualStyleBackColor = false;
+            this.bolme.Click += new System.EventHandler(this.bolme_Click);
+            // 
+            // number7
+            // 
+            this.number7.BackColor = System.Drawing.Color.LightCoral;
+            this.number7.Location = new System.Drawing.Point(13, 199);
+            this.number7.Name = "number7";
+            this.number7.Size = new System.Drawing.Size(51, 50);
+            this.number7.TabIndex = 9;
+            this.number7.Text = "7";
+            this.number7.UseVisualStyleBackColor = false;
+            this.number7.Click += new System.EventHandler(this.number7_Click);
+            // 
+            // number8
+            // 
+            this.number8.BackColor = System.Drawing.Color.LightCoral;
+            this.number8.Location = new System.Drawing.Point(70, 199);
+            this.number8.Name = "number8";
+            this.number8.Size = new System.Drawing.Size(51, 50);
+            this.number8.TabIndex = 10;
+            this.number8.Text = "8";
+            this.number8.UseVisualStyleBackColor = false;
+            this.number8.Click += new System.EventHandler(this.number8_Click);
+            // 
+            // number9
+            // 
+            this.number9.BackColor = System.Drawing.Color.LightCoral;
+            this.number9.Location = new System.Drawing.Point(127, 199);
+            this.number9.Name = "number9";
+            this.number9.Size = new System.Drawing.Size(51, 50);
+            this.number9.TabIndex = 11;
+            this.number9.Text = "9";
+            this.number9.UseVisualStyleBackColor = false;
+            this.number9.Click += new System.EventHandler(this.number9_Click);
+            // 
+            // toplama
+            // 
+            this.toplama.BackColor = System.Drawing.Color.OrangeRed;
+            this.toplama.Location = new System.Drawing.Point(184, 199);
+            this.toplama.Name = "toplama";
+            this.toplama.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.toplama.Size = new System.Drawing.Size(51, 50);
+            this.toplama.TabIndex = 12;
+            this.toplama.Text = "+";
+            this.toplama.UseVisualStyleBackColor = false;
+            this.toplama.Click += new System.EventHandler(this.toplama_Click);
+            // 
+            // temizle
+            // 
+            this.temizle.BackColor = System.Drawing.Color.Red;
+            this.temizle.Location = new System.Drawing.Point(13, 255);
+            this.temizle.Name = "temizle";
+            this.temizle.Size = new System.Drawing.Size(51, 50);
+            this.temizle.TabIndex = 13;
+            this.temizle.Text = "C";
+            this.temizle.UseVisualStyleBackColor = false;
+            this.temizle.Click += new System.EventHandler(this.temizle_Click);
+            // 
+            // number0
+            // 
+            this.number0.BackColor = System.Drawing.Color.LightCoral;
+            this.number0.Location = new System.Drawing.Point(70, 255);
+            this.number0.Name = "number0";
+            this.number0.Size = new System.Drawing.Size(51, 50);
+            this.number0.TabIndex = 14;
+            this.number0.Text = "0";
+            this.number0.UseVisualStyleBackColor = false;
+            this.number0.Click += new System.EventHandler(this.number0_Click);
+            // 
+            // esittir
+            // 
+            this.esittir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.esittir.Location = new System.Drawing.Point(127, 255);
+            this.esittir.Name = "esittir";
+            this.esittir.Size = new System.Drawing.Size(51, 50);
+            this.esittir.TabIndex = 15;
+            this.esittir.Text = "=";
+            this.esittir.UseVisualStyleBackColor = false;
+            this.esittir.Click += new System.EventHandler(this.esittir_Click);
+            // 
+            // cıkarma
+            // 
+            this.cıkarma.BackColor = System.Drawing.Color.OrangeRed;
+            this.cıkarma.Location = new System.Drawing.Point(184, 255);
+            this.cıkarma.Name = "cıkarma";
+            this.cıkarma.Size = new System.Drawing.Size(51, 50);
+            this.cıkarma.TabIndex = 16;
+            this.cıkarma.Text = "-";
+            this.cıkarma.UseVisualStyleBackColor = false;
+            this.cıkarma.Click += new System.EventHandler(this.cıkarma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(246, 333);
+            this.Controls.Add(this.cıkarma);
+            this.Controls.Add(this.esittir);
+            this.Controls.Add(this.number0);
+            this.Controls.Add(this.temizle);
+            this.Controls.Add(this.toplama);
+            this.Controls.Add(this.number9);
+            this.Controls.Add(this.number8);
+            this.Controls.Add(this.number7);
+            this.Controls.Add(this.bolme);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.carpma);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HesapMakinesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button number1;
+        private System.Windows.Forms.Button number2;
+        private System.Windows.Forms.Button number3;
+        private System.Windows.Forms.Button carpma;
+        private System.Windows.Forms.Button number4;
+        private System.Windows.Forms.Button number5;
+        private System.Windows.Forms.Button number6;
+        private System.Windows.Forms.Button bolme;
+        private System.Windows.Forms.Button number7;
+        private System.Windows.Forms.Button number8;
+        private System.Windows.Forms.Button number9;
+        private System.Windows.Forms.Button toplama;
+        private System.Windows.Forms.Button temizle;
+        private System.Windows.Forms.Button number0;
+        private System.Windows.Forms.Button esittir;
+        private System.Windows.Forms.Button cıkarma;
+    }
+}
+
---
+++ HesapMakinesi/HesapMakinesi/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+
+        private int _sayi1, _sayi2, _islem;
+        bool _temizle;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void number1_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "1";
+        }
+
+        private void number2_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "2";
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "3";
+        }
+        private void number4_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "4";
+        }
+
+        private void number5_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "5";
+        }
+
+        private void number6_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "6";
+        }
+
+        private void number7_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+             textBox1.Text += "7";
+        }
+
+        private void number8_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "8";
+        }
+
+        private void number9_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "9";
+        }
+
+        private void number0_Click(object sender, EventArgs e)
+        {
+            if (_temizle == true) textBox1.Text = "";
+            _temizle = false;
+            if (textBox1.Text == "0") textBox1.Text = "";
+            textBox1.Text += "0";
+        }
+
+        private void temizle_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void bolme_Click(object sender, EventArgs e)
+        {
+            _sayi1 = Convert.ToInt32(textBox1.Text);
+            _islem = 4;
+            _temizle = true;
+        }
+
+        private void toplama_Click(object sender, EventArgs e)
+        {
+            _islem = 1;
+            _sayi1 = Convert.ToInt32(textBox1.Text);
+            _temizle = true;
+
+        }
+
+        private void cıkarma_Click(object sender, EventArgs e)
+        {
+            _islem = 2;
+            _sayi1 = Convert.ToInt32(textBox1.Text);
+            _temizle = true;
+        }
+
+        private void carpma_Click(object sender, EventArgs e)
+        {
+            _islem = 3;
+            _sayi1 = Convert.ToInt32(textBox1.Text);
+            _temizle = true;
+
+        }
+        private void esittir_Click(object sender, EventArgs e)
+        {
+            _sayi2 = Convert.ToInt32(textBox1.Text);
+            textBox1.Text = "";
+            switch (_islem)
+            {
+
+                case 1:
+                    textBox1.Text = (_sayi1 + _sayi2).ToString();
+                    break;
+
+                case 2:
+                    textBox1.Text = (_sayi1 - _sayi2).ToString();
+                    break;
+
+                case 3:
+                    textBox1.Text = (_sayi1 * _sayi2).ToString();
+                    break;
+                case 4:
+                    textBox1.Text = (_sayi1 / _sayi2).ToString();
+                    break;
+
+                    
+            }
+
+        }
+
+    }
+}
